4b8b378221202e1a156547719c86caad7bfdf4c8
Ankit-Shakya12 <ankitshakya0210@gmail.com>
2026-03-02 21:29:00 +0530

    Simple_chatbot

---
+++ main.py
@@ -0,0 +1,35 @@
+import os
+from dotenv import load_dotenv
+from typing import Annotated
+from typing_extensions import TypedDict
+from langgraph.graph import StateGraph,START,END
+from langgraph.graph.message import add_messages
+from langchain_groq import ChatGroq
+
+load_dotenv()
+
+llm=ChatGroq(model="openai/gpt-oss-120b")
+
+class State(TypedDict):
+    # Messages have the type "list". The `add_messages` function
+    # in the annotation defines how this state key should be updated
+    # (in this case, it appends messages to the list, rather than overwriting them)
+    messages:Annotated[list,add_messages]
+
+def chatbot(state:State):
+    return {"messages":[llm.invoke(state["messages"])]}
+
+
+graph_builder=StateGraph(State)
+
+## Adding node
+graph_builder.add_node("llmchatbot",chatbot)
+## Adding Edges
+graph_builder.add_edge(START,"llmchatbot")
+graph_builder.add_edge("llmchatbot",END)
+
+## compile the graph
+graph=graph_builder.compile()
+
+response=graph.invoke({"messages":"Hi"})
+print(response)
